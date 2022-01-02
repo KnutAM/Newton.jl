db8e4530f7308414c32c09e1f5dff9a9435cee4a
Knut Andreas Meyer <knutam@gmail.com>
2022-01-02 20:40:30 +0100

    Added solve! tests and reduced allocations

--- src/Newton.jl
+++ src/Newton.jl
@@ -1,5 +1,71 @@
 module Newton
+using LinearAlgebra
+using RecursiveFactorization
+using DiffResults
+using ForwardDiff
 
-# Write your package code here.
+struct NewtonCache{Tres,Tcfg}
+    result::Tres
+    config::Tcfg
+    lupivot::Vector{Int}
+end
+
+function NewtonCache(x::AbstractVector, rf!)
+    result = DiffResults.JacobianResult(x)
+    cfg = ForwardDiff.JacobianConfig(rf!, x, result.value, ForwardDiff.Chunk(length(x)))
+    lupivot = Vector{Int}(undef, length(x))
+    return NewtonCache(result, cfg, lupivot)
+end
+
+"""
+    solve!(K::AbstractMatrix, b::AbstractVector, cache::NewtonCache)
+
+Solves the linear equation system Kx=b, mutating both K and b.
+´b´ is mutated to the solution x
+"""
+function solve!(K::AbstractMatrix, b::AbstractVector, cache::NewtonCache)
+    LU = RecursiveFactorization.lu!(K, cache.lupivot)
+    ldiv!(LU, b)
+    return b
+end
+
+"""
+    newtonsolve!(x::AbstractVector, drdx::AbstractMatrix, rf!, cache::ResidualCache; tol=1.e-6, max_iter=100)
+
+Solve the nonlinear equation system r(x)=0 using the newton method. 
+
+# args
+- `x`: Vector of unknowns. Provide as initial guess, mutated to solution.
+- `drdx`: Jacobian matrix. Only provided as preallocation. Can be aliased to `DiffResults.jacobian(cache.result)`
+- `rf!`: Residual function. Signature `rf!(r, x)` and mutating the residual `r`
+- `cache`: Optional cache that can be preallocated by calling `ResidualCache(x, rf!)`
+
+# kwargs
+- `tol=1.e-6`: Tolerance on `norm(r)`
+- ´maxiter=100`: Maximum number of iterations before no convergence
+
+"""
+
+function newtonsolve!(x::AbstractVector, drdx::AbstractMatrix, rf!, cache::NewtonCache = NewtonCache(x,rf!); tol=1.e-6, max_iter=100)
+    diffresult = cache.result
+    cfg = cache.config
+    for _ = 1:max_iter
+        # Disable checktag using Val{false}(). solve_residual should never be differentiated using dual numbers! 
+        # This is required when using a different (but equivalent) anynomus function for caching than for running.
+        ForwardDiff.jacobian!(diffresult, rf!, diffresult.value, x, cfg, Val{false}())
+        err = norm(diffresult.value)
+        if err < tol
+            drdx .= DiffResults.jacobian(diffresult)
+            return true
+        end
+        solve!(DiffResults.jacobian(diffresult), diffresult.value, cache)
+        x .-= diffresult.value
+    end
+    # No convergence
+    return false
+end
+
+export newtonsolve!
+export NewtonCache
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,39 @@
 using Newton
 using Test
+using ForwardDiff
+using LinearAlgebra
 
-@testset "Newton.jl" begin
-    # Write your tests here.
+@testset "solve!" begin
+    A = 2*I + rand(10,10)
+    b = rand(10)
+    A1, A2 = [copy(A) for _ in 1:2]
+    b1, b2 = [copy(b) for _ in 1:2]
+    rf!(r, x) = (r .= x)
+    @test A1\b1 ≈ Newton.solve!(A2, b2, NewtonCache(b, rf!))
+end
+
+@testset "newtonsolve!" begin
+    nsize = 4
+    (a,b,x0) = [rand(nsize) for _ in 1:3]
+    drdx = zeros(nsize, nsize)
+    tol = 1.e-10
+    
+    # Basic functionality
+    function rf_solution!(r, x)
+        r .= - a + b.*x + exp.(x)
+    end
+    x = copy(x0)
+    converged = newtonsolve!(x, drdx, rf_solution!; tol=tol)
+    r_check = similar(x0)
+    @test converged
+    @test isapprox(rf_solution!(r_check, x), zero(r_check); atol=tol)
+    @test drdx ≈ ForwardDiff.jacobian(rf_solution!, r_check, x)
+
+    function rf_nosolution!(r, x)
+        r .= a .+ b.*x.^2
+    end
+    x = copy(x0)
+    converged = newtonsolve!(x, drdx, rf_nosolution!)
+    @test !converged
+        
 end
