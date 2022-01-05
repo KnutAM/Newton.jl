8d88a0e479bacd182ee4c256dabc8cafda1fb12a
Knut Andreas Meyer <knutam@gmail.com>
2022-01-05 21:10:40 +0100

    Updated docstrings and linsolve! name

--- src/Newton.jl
+++ src/Newton.jl
@@ -10,6 +10,12 @@ struct NewtonCache{Tres,Tcfg}
     lupivot::Vector{Int}
 end
 
+"""
+    function NewtonCache(x::AbstractVector, rf!)
+    
+    Create the cache used by the `newtonsolve!` and `linsolve!` 
+    to find `x` such that `rf!(r,x)` yields `r=0`.
+"""
 function NewtonCache(x::AbstractVector, rf!)
     result = DiffResults.JacobianResult(x)
     cfg = ForwardDiff.JacobianConfig(rf!, x, result.value, ForwardDiff.Chunk(length(x)))
@@ -17,15 +23,19 @@ function NewtonCache(x::AbstractVector, rf!)
     return NewtonCache(result, cfg, lupivot)
 end
 
+"""
+    get_drdx(cache::NewtonCache) = DiffResults.jacobian(cache.result)    
+
+"""
 get_drdx(cache::NewtonCache) = DiffResults.jacobian(cache.result)
 
 """
-    solve!(K::AbstractMatrix, b::AbstractVector, cache::NewtonCache)
+    linsolve!(K::AbstractMatrix, b::AbstractVector, cache::NewtonCache)
 
-Solves the linear equation system Kx=b, mutating both K and b.
-´b´ is mutated to the solution x
+Solves the linear equation system `Kx=b`, mutating both `K` and `b`.
+`b` is mutated to the solution `x`
 """
-function solve!(K::AbstractMatrix, b::AbstractVector, cache::NewtonCache)
+function linsolve!(K::AbstractMatrix, b::AbstractVector, cache::NewtonCache)
     LU = RecursiveFactorization.lu!(K, cache.lupivot)
     ldiv!(LU, b)
     return b
@@ -34,7 +44,7 @@ end
 """
     newtonsolve!(x::AbstractVector, drdx::AbstractMatrix, rf!, cache::ResidualCache; tol=1.e-6, max_iter=100)
 
-Solve the nonlinear equation system r(x)=0 using the newton method. 
+Solve the nonlinear equation system r(x)=0 using the newton-raphson method. Returns `true` if converged and `false` otherwise.
 
 # args
 - `x`: Vector of unknowns. Provide as initial guess, mutated to solution.
@@ -44,10 +54,9 @@ Solve the nonlinear equation system r(x)=0 using the newton method.
 
 # kwargs
 - `tol=1.e-6`: Tolerance on `norm(r)`
-- ´maxiter=100`: Maximum number of iterations before no convergence
+- `maxiter=100`: Maximum number of iterations before no convergence
 
 """
-
 function newtonsolve!(x::AbstractVector, drdx::AbstractMatrix, rf!, cache::NewtonCache = NewtonCache(x,rf!); tol=1.e-6, max_iter=100)
     diffresult = cache.result
     cfg = cache.config
@@ -60,7 +69,7 @@ function newtonsolve!(x::AbstractVector, drdx::AbstractMatrix, rf!, cache::Newto
             drdx .= DiffResults.jacobian(diffresult)
             return true
         end
-        solve!(DiffResults.jacobian(diffresult), diffresult.value, cache)
+        linsolve!(DiffResults.jacobian(diffresult), diffresult.value, cache)
         x .-= diffresult.value
     end
     # No convergence
